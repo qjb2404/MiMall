a978b279089d1bc25061964d4147dc4ca66bca48
qiaojiangbo <907114069@qq.com>
2020-01-29 12:48:22 +0800

    mock

--- src/App.vue
+++ src/App.vue
@@ -13,15 +13,22 @@ export default {
   },
   data(){
     return {
-      res:{}
+      //res:{}
     }
   },
   mounted(){
     //本地请求加载静态json文件的形式
-    this.axios.get('/mock/user/login.json').then((res)=>{
-      this.res = res;
-    })
-    
+    //this.axios.get('/mock/user/login.json').then((res)=>{
+      //this.res = res;
+    //})
+    //通过easy-mock平台实现数据mock
+    //this.axios.get('/user/login').then((res)=>{
+      //this.res = res;
+    //})
+    //本地集成mockjs实现数据mock
+    //this.axios.get('/user/login').then((res)=>{
+      //this.res = res;
+    //})
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -5,7 +5,15 @@ import VueAxios from 'vue-axios'
 import App from './App.vue'
 // import env from './env'
 
+
+// mock开关
+const mock = false;
+if (mock) {
+    require('./mock/api');
+}
+// 根据前端的跨域方式做调整
 //axios.defaults.baseURL = '/api';
+axios.defaults.baseURL = 'https://www.easy-mock.com/mock/5dc7afee2b69d9223b633cbb/mimall';
 axios.defaults.timeout = 8000;
 // 根据环境变量获取不同的请求地址
 // axios.defaults.baseURL = env.baseURL;
